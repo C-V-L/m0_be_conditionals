c5b643d028cfac7b8bab538937223a2590ef13cf
Conner Van Loan <cdvanloan@gmail.com>
2022-11-02 14:07:17 -0600

    Add spicy.rb and answers

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,35 @@
+
+soda = 15
+
+if (soda%5).zero? && (soda%3).zero?
+    puts "FizzBuzz"
+elsif (soda%5).zero? 
+    puts "Buzz"
+elsif (soda%3).zero?
+            puts "Fizz"
+end
+
+
+
+soda = 20
+
+if (soda%5).zero? && (soda%3).zero?
+    puts "FizzBuzz"
+elsif (soda%5).zero? 
+        puts "Buzz"
+elsif (soda%3).zero?
+            puts "Fizz"
+end
+
+
+soda = 18
+
+if (soda%5).zero? && (soda%3).zero?
+    puts "FizzBuzz"
+elsif (soda%5).zero? 
+        puts "Buzz"
+elsif (soda%3).zero?
+            puts "Fizz"
+end
+
+
